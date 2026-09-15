40a1d827ec19d115321a574bfdec038542513593
salim-dev-98 <salim@oip.com.pk>
2026-09-15 22:12:29 +0500

    code updated

--- src/components/Industries.tsx
+++ src/components/Industries.tsx
@@ -1,7 +1,6 @@
 import Icon from './Icon';
 import { useReveal } from '../hooks/useReveal';
 import arrowRightSvg from '../assets/icons/boxicons-arrow-right-stroke.svg?raw';
-import orbGlow from '../assets/images/orb-sphere.webp';
 import orbGlowSoft from '../assets/images/industries-glow-soft.png';
 import healthcareSvg from '../assets/icons/healthcare.svg?raw';
 import dentalSvg from '../assets/icons/dental.svg?raw';
@@ -50,9 +49,6 @@ export default function Industries() {
     <section className="industries section">
       <div className="industries__glow-wrap">
         <img src={orbGlowSoft} alt="" className="industries__glow industries__glow--soft" />
-        <div className="industries__glow-spin">
-          <img src={orbGlow} alt="" className="industries__glow industries__glow--sharp" />
-        </div>
       </div>
 
       <div className="container">
